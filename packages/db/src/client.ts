7460c10cff30db4b1f433d0e6d372da834923e88
Yang <19787969@student.westernsydney.edu.au>
2026-05-26 17:29:10 +1000

    revert change

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
+import { env } from "@repo/env/web";
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -10,12 +10,7 @@ export const createClient = () => {
     return global.prisma;
   }
 
-  const URL = process.env.DATABASE_URL;
-  if (!URL) {
-    throw new Error(
-      "DATABASE_URL is not set. Set it in environment or packages/db/.env",
-    );
-  }
+  const URL = env.DATABASE_URL;
 
   const prisma = new PrismaClient({
     datasourceUrl: URL,
